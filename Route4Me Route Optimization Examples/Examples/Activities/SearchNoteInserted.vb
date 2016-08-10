b1d9cfb3e30edbebb8d20a81859646437009ab0f
Amazos <olegi_g@yahoo.com>
2016-08-10 20:09:24 +0400

    added SearchNoteInserted example

---
+++ Route4Me Route Optimization Examples/Examples/Activities/SearchNoteInserted.vb
@@ -0,0 +1,35 @@
+﻿Imports Route4MeSDKLibrary.Route4MeSDK
+Imports Route4MeSDKLibrary.Route4MeSDK.DataTypes
+Imports Route4MeSDKLibrary.Route4MeSDK.QueryTypes
+Namespace Route4MeSDKTest.Examples
+    Partial Public NotInheritable Class Route4MeExamples
+        Public Sub SearchNoteInserted()
+            ' Create the manager with the api key
+            Dim route4Me As New Route4MeManager(c_ApiKey)
+
+            Dim activityParameters As New ActivityParameters() With { _
+                .ActivityType = "note-insert", _
+                .RouteId = "C3E7FD2F8775526674AE5FD83E25B88A" _
+            }
+
+            ' Run the query
+            Dim errorString As String = ""
+            Dim activities As Activity() = route4Me.GetActivityFeed(activityParameters, errorString)
+
+            Console.WriteLine("")
+
+            If activities IsNot Nothing Then
+                Console.WriteLine("SearchNoteInserted executed successfully, {0} activities returned", activities.Length)
+                Console.WriteLine("")
+
+                For Each Activity As Activity In activities
+                    Console.WriteLine("Activity ID: {0}", Activity.ActivityId)
+                Next
+
+                Console.WriteLine("")
+            Else
+                Console.WriteLine("SearchNoteInserted error: {0}", errorString)
+            End If
+        End Sub
+    End Class
+End Namespace
