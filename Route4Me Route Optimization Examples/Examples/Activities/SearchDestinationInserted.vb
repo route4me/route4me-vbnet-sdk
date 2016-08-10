eb2815b55447e49274272007a815204b8f57b5fd
Amazos <olegi_g@yahoo.com>
2016-08-10 19:35:57 +0400

    added SearchDestnationInserted example

---
+++ Route4Me Route Optimization Examples/Examples/Activities/SearchDestinationInserted.vb
@@ -0,0 +1,35 @@
+﻿Imports Route4MeSDKLibrary.Route4MeSDK
+Imports Route4MeSDKLibrary.Route4MeSDK.DataTypes
+Imports Route4MeSDKLibrary.Route4MeSDK.QueryTypes
+Namespace Route4MeSDKTest.Examples
+    Partial Public NotInheritable Class Route4MeExamples
+        Public Sub SearchDestinationInserted()
+            ' Create the manager with the api key
+            Dim route4Me As New Route4MeManager(c_ApiKey)
+
+            Dim activityParameters As New ActivityParameters() With { _
+                .ActivityType = "insert-destination", _
+                .RouteId = "87B8873BAEA4E09942C68E2C92A9C4B7" _
+            }
+
+            ' Run the query
+            Dim errorString As String = ""
+            Dim activities As Activity() = route4Me.GetActivityFeed(activityParameters, errorString)
+
+            Console.WriteLine("")
+
+            If activities IsNot Nothing Then
+                Console.WriteLine("SearchDestinationInserted executed successfully, {0} activities returned", activities.Length)
+                Console.WriteLine("")
+
+                For Each Activity As Activity In activities
+                    Console.WriteLine("Activity ID: {0}", Activity.ActivityId)
+                Next
+
+                Console.WriteLine("")
+            Else
+                Console.WriteLine("SearchDestinationInserted error: {0}", errorString)
+            End If
+        End Sub
+    End Class
+End Namespace
