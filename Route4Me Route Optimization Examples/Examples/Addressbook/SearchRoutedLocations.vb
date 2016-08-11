da1c92a049172fc379eb4637520bf6ddaa51388b
Amazos <olegi_g@yahoo.com>
2016-08-11 17:30:54 +0400

    added SearchRoutedLocations example

---
+++ Route4Me Route Optimization Examples/Examples/Addressbook/SearchRoutedLocations.vb
@@ -0,0 +1,32 @@
+﻿Imports Route4MeSDKLibrary.Route4MeSDK
+Imports Route4MeSDKLibrary.Route4MeSDK.DataTypes
+Imports Route4MeSDKLibrary.Route4MeSDK.QueryTypes
+Namespace Route4MeSDKTest.Examples
+    Partial Public NotInheritable Class Route4MeExamples
+        Public Sub SearchRoutedLocations()
+            ' Create the manager with the api key
+            Dim route4Me As New Route4MeManager(c_ApiKey)
+
+            Dim addressBookParameters As New AddressBookParameters() With { _
+                .Display = "routed", _
+                .Offset = 0, _
+                .Limit = 20 _
+            }
+
+            ' Run the query
+            Dim total As UInteger
+            Dim errorString As String = ""
+            Dim contacts As AddressBookContact() = route4Me.GetAddressBookLocation(addressBookParameters, total, errorString)
+
+            Console.WriteLine("")
+
+            If contacts IsNot Nothing Then
+                Console.WriteLine("SearchRoutedLocations executed successfully, {0} contacts returned, total = {1}", contacts.Length, total)
+
+                Console.WriteLine("")
+            Else
+                Console.WriteLine("SearchRoutedLocations error: {0}", errorString)
+            End If
+        End Sub
+    End Class
+End Namespace
