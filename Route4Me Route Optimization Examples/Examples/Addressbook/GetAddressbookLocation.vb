e0a69dd3310b718f909aa5657ae8209bb6b48d21
Amazos <olegi_g@yahoo.com>
2016-08-11 16:03:43 +0400

    added GetAddressbookLocation examples

---
+++ Route4Me Route Optimization Examples/Examples/Addressbook/GetAddressbookLocation.vb
@@ -0,0 +1,33 @@
+﻿Imports Route4MeSDKLibrary.Route4MeSDK
+Imports Route4MeSDKLibrary.Route4MeSDK.DataTypes
+Imports Route4MeSDKLibrary.Route4MeSDK.QueryTypes
+Namespace Route4MeSDKTest.Examples
+    Partial Public NotInheritable Class Route4MeExamples
+        Public Sub GetAddressbookLocation()
+            ' Create the manager with the api key
+            Dim route4Me As New Route4MeManager(c_ApiKey)
+
+            Dim addressBookParameters As New AddressBookParameters() With { _
+                .Query = "david", _
+                .Offset = 0, _
+                .Limit = 20 _
+            }
+
+            ' Run the query
+            Dim total As UInteger
+            Dim errorString As String = ""
+            Dim contacts As AddressBookContact() = route4Me.GetAddressBookLocation(addressBookParameters, total, errorString)
+
+            Console.WriteLine("")
+
+            If contacts IsNot Nothing Then
+                Console.WriteLine("GetAddressbookLocation executed successfully, {0} contacts returned, total = {1}", contacts.Length, total)
+
+                Console.WriteLine("")
+            Else
+                Console.WriteLine("GetAddressbookLocation error: {0}", errorString)
+            End If
+        End Sub
+    End Class
+End Namespace
+
