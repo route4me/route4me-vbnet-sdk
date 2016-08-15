3925487c5b3c4a87791652cf35ba0e3a6ed41baf
Amazos <olegi_g@yahoo.com>
2016-08-15 12:49:45 +0400

    added Territories group example

---
+++ Route4Me Route Optimization Examples/Examples/Territories/CreateTerritory.vb
@@ -0,0 +1,38 @@
+﻿Imports Route4MeSDKLibrary.Route4MeSDK
+Imports Route4MeSDKLibrary.Route4MeSDK.DataTypes
+Imports Route4MeSDKLibrary.Route4MeSDK.QueryTypes
+Namespace Route4MeSDKTest.Examples
+    Partial Public NotInheritable Class Route4MeExamples
+        ''' <summary>
+        ''' Add Avoidance Zone
+        ''' </summary>
+        Public Sub CreateTerritory()
+            ' Create the manager with the api key
+            Dim route4Me As New Route4MeManager(c_ApiKey)
+
+            Dim territoryParameters As New AvoidanceZoneParameters() With { _
+                .TerritoryName = "Test Territory", _
+                .TerritoryColor = "ff0000", _
+                .Territory = New Territory() With { _
+                    .Type = EnumHelper.GetEnumDescription(TerritoryType.Circle), _
+                    .Data = New String() {"37.569752822786455,-77.47833251953125", "5000"} _
+                } _
+            }
+
+            ' Run the query
+            Dim errorString As String = ""
+            Dim territory As AvoidanceZone = route4Me.AddAvoidanceZone(territoryParameters, errorString)
+
+            Console.WriteLine("")
+
+            If territory IsNot Nothing Then
+                Console.WriteLine("CreateTerritory executed successfully")
+
+                Console.WriteLine("Territory ID: {0}", territory.TerritoryId)
+            Else
+                Console.WriteLine("CreateTerritory error: {0}", errorString)
+            End If
+        End Sub
+    End Class
+End Namespace
+
