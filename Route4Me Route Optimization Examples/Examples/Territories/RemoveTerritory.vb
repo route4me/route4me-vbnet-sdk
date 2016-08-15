e7e04de8a8c5a2f591cb1f4de798fcfa29f716ca
Amazos <olegi_g@yahoo.com>
2016-08-15 16:05:09 +0400

    added Territories group example

---
+++ Route4Me Route Optimization Examples/Examples/Territories/RemoveTerritory.vb
@@ -0,0 +1,34 @@
+﻿Imports Route4MeSDKLibrary.Route4MeSDK
+Imports Route4MeSDKLibrary.Route4MeSDK.DataTypes
+Imports Route4MeSDKLibrary.Route4MeSDK.QueryTypes
+Namespace Route4MeSDKTest.Examples
+    Partial Public NotInheritable Class Route4MeExamples
+        ''' <summary>
+        ''' Remove Territory
+        ''' </summary>
+        Public Sub RemoveTerritory()
+            ' Create the manager with the api key
+            Dim route4Me As New Route4MeManager(c_ApiKey)
+
+            Dim territoryId As String = "12ABBFA3B5E4FB007BB0ED73291576C2"
+
+            Dim territoryQuery As New AvoidanceZoneQuery() With { _
+                .TerritoryId = territoryId _
+            }
+
+            ' Run the query
+            Dim errorString As String = ""
+            route4Me.RemoveTerritory(territoryQuery, errorString)
+
+            Console.WriteLine("")
+
+            If errorString = "" Then
+                Console.WriteLine("RemoveTerritory executed successfully")
+
+                Console.WriteLine("Territory ID: {0}", territoryId)
+            Else
+                Console.WriteLine("RemoveTerritory error: {0}", errorString)
+            End If
+        End Sub
+    End Class
+End Namespace
