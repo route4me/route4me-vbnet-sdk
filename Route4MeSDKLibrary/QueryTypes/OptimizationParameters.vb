71b5a344504422c28cadba2b5c53f36a3b692f6b
Amazos <olegi_g@yahoo.com>
2019-06-16 22:48:33 +0400

    updated vb.net sdk

--- Route4MeSDKLibrary/QueryTypes/OptimizationParameters.vb
+++ Route4MeSDKLibrary/QueryTypes/OptimizationParameters.vb
@@ -11,27 +11,11 @@ Namespace Route4MeSDK.QueryTypes
         <IgnoreDataMember> _
         <HttpQueryMemberAttribute(Name:="optimization_problem_id", EmitDefaultValue:=False)> _
         Public Property OptimizationProblemID() As String
-            Get
-                Return m_OptimizationProblemID
-            End Get
-            Set(value As String)
-                m_OptimizationProblemID = Value
-            End Set
-        End Property
-        Private m_OptimizationProblemID As String
 
         ' Don't serialize as JSON
         <IgnoreDataMember> _
         <HttpQueryMemberAttribute(Name:="reoptimize", EmitDefaultValue:=False)> _
         Public Property ReOptimize() As System.Nullable(Of Boolean)
-            Get
-                Return m_ReOptimize
-            End Get
-            Set(value As System.Nullable(Of Boolean))
-                m_ReOptimize = Value
-            End Set
-        End Property
-        Private m_ReOptimize As System.Nullable(Of Boolean)
 
         ''' <summary>
         ''' If true will be redirected
@@ -39,48 +23,21 @@ Namespace Route4MeSDK.QueryTypes
         <IgnoreDataMember> _
         <HttpQueryMemberAttribute(Name:="redirect", EmitDefaultValue:=False)> _
         Public Property Redirect() As System.Nullable(Of Boolean)
-            Get
-                Return m_Redirect
-            End Get
-            Set(value As System.Nullable(Of Boolean))
-                m_Redirect = value
-            End Set
-        End Property
-        Private m_Redirect As System.Nullable(Of Boolean)
 
         ' Don't serialize as JSON
         <IgnoreDataMember> _
         <HttpQueryMemberAttribute(Name:="show_directions", EmitDefaultValue:=False)> _
         Public Property ShowDirections() As System.Nullable(Of Boolean)
-            Get
-                Return m_ShowDirections
-            End Get
-            Set(value As System.Nullable(Of Boolean))
-                m_ShowDirections = Value
-            End Set
-        End Property
-        Private m_ShowDirections As System.Nullable(Of Boolean)
+
+        <IgnoreDataMember> _
+        <HttpQueryMemberAttribute(Name:="optimized_callback_url", EmitDefaultValue:=False)> _
+        Public Property OptimizedCallbackURL() As String
 
         <DataMember(Name:="parameters", EmitDefaultValue:=False)> _
         Public Property Parameters() As RouteParameters
-            Get
-                Return m_Parameters
-            End Get
-            Set(value As RouteParameters)
-                m_Parameters = Value
-            End Set
-        End Property
-        Private m_Parameters As RouteParameters
 
         <DataMember(Name:="addresses", EmitDefaultValue:=False)> _
         Public Property Addresses() As Address()
-            Get
-                Return m_Addresses
-            End Get
-            Set(value As Address())
-                m_Addresses = Value
-            End Set
-        End Property
-        Private m_Addresses As Address()
+
     End Class
 End Namespace
